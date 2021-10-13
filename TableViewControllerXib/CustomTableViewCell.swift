97d74496e3a088ee890ec2aa55bdeeebf0a1fc7c
Alivia <withalivia@gmail.com>
2021-10-12 18:56:40 -0700

    Checking in Table View with Nib

---
+++ TableViewControllerXib/CustomTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CustomTableViewCell.swift
+//  TableViewControllerXib
+//
+//  Created by Alivia Guin on 10/12/21.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var lblImage: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- TableViewControllerXib/ViewController.swift
+++ TableViewControllerXib/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let imageArray = ["Seattle1", "Seattle2", "Seattle3", "Seattle4", "Seattle5", "Seattle6", "Seattle7", "Seattle8", "Seattle9", "Seattle10"]
 
+    @IBOutlet weak var tblView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Bundle.main.loadNibNamed("CustomTableViewCell", owner: self, options: nil)?.first as! CustomTableViewCell
+        cell.imgView.image = UIImage(named: imageArray[indexPath.row])
+        cell.lblImage.text = imageArray[indexPath.row]
 
+        return cell
+    }
 
 }
 
